daa6c27c87cb6abf433d89858bddfd750d07493d
cucea <jjroblesg@hotmail.com>
2024-02-21 21:07:44 -0600

    incluir gráficos

--- app/src/main/java/com/example/lostobjects/ListaDeObjetosActivity.kt
+++ app/src/main/java/com/example/lostobjects/ListaDeObjetosActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lostobjects.ui.theme.LostObjectsTheme
 import com.example.objetosperdidos.ObjetoPerdido
 import com.google.firebase.Firebase
@@ -39,10 +55,12 @@ class ListaDeObjetosActivity : ComponentActivity() {
 
 @Composable
 fun ListarObjetos() {
+    var listaDeObjetos: List<ObjetoPerdido?> by remember { mutableStateOf(emptyList()) }
+    var objetosPerdidos = mutableListOf<ObjetoPerdido?>()
+
     val TAG = "Lista de Objetos"
     val database = Firebase.database
     val myRef = database.getReference("objetosPerdidos")
-    var objetosPerdidos: ArrayList<ObjetoPerdido?> = ArrayList()
     myRef.addValueEventListener(object: ValueEventListener {
 
         override fun onDataChange(snapshot: DataSnapshot) {
@@ -56,7 +74,7 @@ fun ListarObjetos() {
                         objetoPerdido?.ubicacion)
                 objetosPerdidos.add(objetoPerdido)
             }
-
+            listaDeObjetos = objetosPerdidos
 
         }
 
@@ -65,13 +83,50 @@ fun ListarObjetos() {
         }
 
     })
+    imprimirObjetos(listaDeObjetos)
 
 }
+@Composable
+fun imprimirObjetos(lista: List<ObjetoPerdido?>){
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.linearGradient(
+                listOf(
+                    Color(0xFFacfaf5),
+                    Color(0xFFe6f7f6)
+                ))
+            )
+            .padding(40.dp),
+
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        items(lista.size){
+            indice ->
+                var op = lista[indice]
+                Row(){
+                    Text(""+op?.descripcion,
+                        fontFamily = FontFamily.Monospace,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Blue,
+                        modifier = Modifier.padding(10.dp)
+
+                    )
+                    Text(""+op?.ubicacion,
+                        modifier = Modifier.padding(10.dp))
+                }
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview2() {
     LostObjectsTheme {
-        ListarObjetos()
+
+        imprimirObjetos(emptyList())
     }
 }
--- app/src/main/java/com/example/lostobjects/MainActivity.kt
+++ app/src/main/java/com/example/lostobjects/MainActivity.kt
@@ -6,14 +6,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -25,6 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -55,16 +59,51 @@ class MainActivity : ComponentActivity() {
         var descripcionObjetoPerdido by remember { mutableStateOf("") }
         var ubicacion by remember { mutableStateOf("") }
 
-        Column (){
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Brush.linearGradient(
+                    listOf(
+                        Color(0xFFacfaf5),
+                        Color(0xFFe6f7f6)))
+                )
+                .padding(40.dp),
+
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text("Objeto")
             BasicTextField(
                 value = descripcionObjetoPerdido,
-                onValueChange = {descripcionObjetoPerdido = it}
+                onValueChange = {descripcionObjetoPerdido = it},
+                modifier = Modifier
+                    .padding(5.dp)
+                    .border(2.dp, Color.Black)
+                    .background(Color.White)
+                    .height(30.dp)
+                    .fillMaxWidth()
+
             )
+
+            Text("", modifier = Modifier.padding(10.dp))
+            Text("Ubicación")
             BasicTextField(
                 value = ubicacion,
-                onValueChange = {ubicacion = it}
+                onValueChange = {ubicacion = it},
+                modifier = Modifier
+                    .padding(5.dp)
+                    .border(2.dp, Color.Black)
+                    .background(Color.White)
+                    .height(30.dp)
+                    .fillMaxWidth()
+
             )
-            Button(onClick = {
+            Button(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(Color.Black),
+                onClick = {
                 var objetoPerdido = ObjetoPerdido()
                 objetoPerdido.descripcion = descripcionObjetoPerdido
                 objetoPerdido.ubicacion = ubicacion
@@ -79,7 +118,12 @@ class MainActivity : ComponentActivity() {
             }) {
                 Text(text = "Agregar")
             }
-            Button(onClick = {
+            Button(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(Color.Black),
+                onClick = {
                     var intent = Intent(this@MainActivity,
                         ListaDeObjetosActivity::class.java)
                     startActivity(intent)
